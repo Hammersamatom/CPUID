ecbf44d6f6c8b8b476efb2cce39c51928258d0b9
Hammersamatom <hammersamatom@gmail.com>
2020-03-27 00:36:03 -0400

    Different and quicker method of getting register information.

--- main.c
+++ main.c
@@ -1,59 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* spaceBytes(char* input, int size)
+
+char* IntToCharPointer(int input)
 {
-  int newSize = size + size/8;        // Specifying new size for output array. The subtraction is to avoid the first space from the array.
+    int totalChars = sizeof(input);
+    char* output = (char*)malloc(totalChars);
 
-  char* output = (char*)malloc(newSize);
-
-  int offset = 0;
-  for (int i = 0; i < size; i++)
-  {
-    if (i % 8 == 0/* && i != 0*/)
+    for (int byte = 0; byte < totalChars; byte++)
     {
-      output[i+offset] = ' ';
-      offset++; 
+        /*
+        for (int bit = 0; bit < 8; bit++)
+        {
+            int stepper = bit + (byte * 8);
+            int shifted = 1 << stepper;
+            int ordbyte = (input & shifted) >> byte*8;
+            output[(totalChars-1)-byte] |= ordbyte;  
+        }
+        */
+        int stepper = 8 * byte;
+        int shifted = 0xff << stepper;
+        int ordbyte = (input & shifted) >> stepper;
+        output[(totalChars-1)-byte] |= ordbyte;
     }
 
-    output[i+offset] = input[i];
-  }
+    return output;
+}                                                            
 
-  return output;
+
+char* ReverseCharPointer(char* array, int size)
+{
+    char* output = (char*)malloc(size);
+
+    for (int i = 0; i < size; i++)
+    {
+        output[(size-1)-i] = array[i];
+    }
+
+    return output;
 }
 
+
+// Spaces out a char* every 8th character
+char* spaceBytes(char* input, int size)
+{
+    int newSize = size + size/8;        // Specifying new size for output array. The subtraction is to avoid the first space from the array.
+
+    char* output = (char*)malloc(newSize);
+
+    int offset = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (i % 8 == 0)
+        {
+            output[i+offset] = ' ';
+            offset++; 
+        }
+
+        output[i+offset] = input[i];
+    }
+    
+    return output;
+}
+
+
+// Takes a INT32 as an input and outputs it's binary equivalent as characters in a char*
 char* binary(int input, int size)
 {
 	char* output = (char*)malloc(size);
 	
-  for (int i = 0; i < size; i++)
-  {
-      output[(size-1)-i] = (input & (1 << i)) >> i ? '1' : '0';    
-  }
+    for (int i = 0; i < size; i++)
+    {
+        output[(size-1)-i] = (input & (1 << i)) >> i ? '1' : '0';    
+    }
 
 	return output;
 }
 
+
+
+// Spaces a string output every 8th character <<<< This functions like a macro and doesn't have to exist >>>>
 char* getNiceBytes(int input, int size)
 {
-  return spaceBytes(binary(input, size), size);
+    return spaceBytes(binary(input, size), size);
+}
+
+void cpuid(int address, int* registers)
+{
+    __asm__(
+            "cpuid"
+            :"=a"(registers[0]), "=b"(registers[1]), "=c"(registers[2]), "=d"(registers[3]) // Assigning 
+            :"0"(address) // Address into EAX
+           );
 }
 
 int main()
 {
-  int i, a, b, c, d;
+    int regNum = 4; // EAX, EBX, ECX, EDX
+    int* regs = (int*)malloc(4);
+    char* processor = (char*)malloc(48);
+    
 
-  for (i = 0; i < 8; i++)
-  {
-    __asm__(
-            "cpuid"
-            :"=a"(a), "=b"(b), "=c"(c), "=d"(d) // EAX into a (output)
-            :"0"(0x80000000+i)                  // i into EAX (input)
-            //:"%ecx","%edx"                    // clobbered registers are unused or trashed by other instructions
-            ); 
+    for (int i = 0; i < 3; i++)
+    {
+        int offset = 0x80000002 + i;
+        cpuid(offset, regs); 
+        /*                                                                                                                                                                                  
+        printf("%Xh:(EAX) %s (EBX) %s (ECX) %s (EDX) %s\n", offset, ReverseCharPointer(IntToCharPointer(regs[0]), 4),
+                                                                    ReverseCharPointer(IntToCharPointer(regs[1]), 4),
+                                                                    ReverseCharPointer(IntToCharPointer(regs[2]), 4),
+                                                                    ReverseCharPointer(IntToCharPointer(regs[3]), 4));
+        */
+        for (int j = 0; j < 16; j++)
+        {
+            processor[j+(i*16)] = ReverseCharPointer(IntToCharPointer(regs[j/4]), 4)[j%4];
+        }
+    }
 
-    printf("The code %i gives >> (EAX)%s (EBX)%s (ECX)%s (EDX)%s\n", i, getNiceBytes(a, 32), getNiceBytes(b, 32), getNiceBytes(c, 32), getNiceBytes(d, 32));
-  }
-  
-  return 0;
+    printf("String: %s\n", processor);
+    return 0;
 }
